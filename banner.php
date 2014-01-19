23a138d68c5b44117d31b151b6bde2b247ec3acb
Marcin Szczodrak <msz@cs.columbia.edu>
2014-01-19 15:26:07 -0500

    update banner

--- banner.php
+++ banner.php
@@ -4,4 +4,13 @@
 		<img src="pics/cu_01.jpg" width="100%">
 	</td>
 	</tr>
+	<tr>
+	<td align="center">
+		<br>
+		<font size=+2 face="Sans">
+			SmartCity Research Project at Columbia University
+		</font>
+		<br>
+	</td>
+	</tr>
 </table>
