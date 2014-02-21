c18ddf7fb5c36090fc09c3d8b151e64afa2a97ef
Marcin Szczodrak <msz@cs.columbia.edu>
2014-02-21 16:58:32 -0500

    update project description

--- projects.php
+++ projects.php
@@ -1,25 +1,90 @@
-<br><br><br><br>
+<br><br>
 <table>
 	<tr>
 
 	<td width="33%" align="center">
+		<img src="./pics/fennec_swift.png" width="90%">
+	</td>
+
+	<td width="33.5%" align="center">
 		<a href="https://github.com/mszczodrak/fennec-fox/wiki">
-			<img src="./pics/fennec_fox_button.png" width="90%">
+			<img src="./pics/fennec_fox_button.png" width="80%">
 		</a>
 	</td>
 
-	<td width="33%" align="center">
+	<td width="33.5%" align="center">
 		<a href="https://github.com/mszczodrak/swift-fox/wiki">
-			<img src="./pics/swift_fox_button.png" width="90%">
+			<img src="./pics/swift_fox_button.png" width="80%">
+		</a>
+	</td>
+	</tr>
+
+	<tr>
+
+	<td width="33%" align="center">
+		<font size="+2">
+		System Design and Programming
+		</font>
+	</td>
+
+	<td colspan="2">
+		<font size="-1">
+		Fennec Fox addresses the need of running multiple heterogenous
+		processes on the same Wireless Sensor Network. This can be used
+		to run multiple applications on the same network, or to desing
+		complex applications as a composition of multiple processes.
+		Swift Fox is a programming language to model the execution
+		of the network processes executing on a Fennec Fox system.
+		</font>
+	</td>
+	</tr>
+
+	<tr>
+	<td colspan="3">
+		<br><br><br>
+	</td>
+	</tr>
+	
+	<tr>
+
+	<td width="33%" align="center">
+		<img src="./pics/tools.png" width="90%">
+	</td>
+
+	<td width="33%" align="center">
+		<a href="https://github.com/mszczodrak/fennec-fox/wiki/Cape%20Fox-Simulator">
+			<img src="./pics/cape_fox_button.png" width="80%">
 		</a>
 	</td>
 
 	<td width="33%" align="center">
 		<a href="https://github.com/mszczodrak/otf/wiki">
-			<img src="./pics/otf_button.png" width="90%">
+			<img src="./pics/otf_button.png" width="80%">
 		</a>
 	</td>
 
+	</tr>
+
+	<tr>
+
+	<td width="33%" align="center">
+		<font size="+2">
+		System Testing and Installing
+		</font>
+	</td>
+
+	<td colspan="2">
+		<font size="-1">
+		Cape Fox is a simulator that allows to test a Wireless Sensor
+		Network system running with Fennec Fox and programmed with
+		Swift Fox. It support developing and testing of Cyber-Physical
+		Systems' applications, by enabling real-time excution with
+		real-time sensor and actuator measurements.
+		Open Testbed Framework provides a set of comprehensive tools
+		to deploy, test, and debug an embedded firmware executing
+		on an actual wireless network of embedded sensors and actuators.
+		</font>
+	</td>
 	</tr>
 </table>
 
