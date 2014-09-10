2503d29e678284f4cbaf387050e4f376e007e046
Marcin Szczodrak <msz@cs.columbia.edu>
2014-09-10 09:48:00 -0400

    update website

--- top.php
+++ top.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta name="description" content="Smart City Columbia Univrsity" />
-<meta name="keywords" content="Marcin,Szczodrak,TinyOS,Embedded,System,Wireless,Network,Columbia,Research,Fennec,Swift,Cape,Low Power,WSN, LPWN, CPS, Cyber-Physical Systems, Fennec Fox, Swift Fox, Cape Fox, testbed, tools, Luca, Carloni" />
+<meta name="keywords" content="Fennec,Fox,Marcin,Szczodrak,Columbia,Columbia University,TinyOS,FreeRTOS,Contiki,Embedded,System,Wireless,Network,Research,Fennec,Swift,Cape,Low Power,WSN, LPWN, CPS, Cyber-Physical Systems, Fennec Fox, Swift Fox, Cape Fox, testbed, tools, Luca, Carloni" />
 <meta name="author" content="Marcin Szczodrak" />
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <title>SmartCity @ Columbia University</title>
